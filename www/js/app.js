cc625d41dcafe8e6697fc7d9d0be19604c922a4a
AnthonyCorrado <amcstudios@yahoo.com>
2014-10-13 19:36:39 -0700

    added simple menus

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,15 @@
-// Ionic Starter App
+app = angular.module('todo', ['ionic']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+app.controller('TodoCtrl', ['$scope', function($scope) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+}]);
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,4 +20,4 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
